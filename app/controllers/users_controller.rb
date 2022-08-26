4fe4f9c04a3fc567fcf0bec37feceacd94061152
fateskink <shaynenguyen9@gmail.com>
2022-08-26 15:38:31 +0700

    chap 7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome, U mazer Fuckah"
+      redirect_to user_url (@user)
     else
       render 'new'
     end
