941bc3b457ee4ab06bde63f8c74a3531bc0b9332
fateskink <shaynenguyen9@gmail.com>
2022-08-26 12:03:46 +0700

    lunch 2608

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
+  # get '/show', to: 'users#show'
+  resources :users
   # root 'application#hello'
 end
