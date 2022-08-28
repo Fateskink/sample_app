d3a7d068f2c0c29aa4e978d399d585e6e2ea8407
fateskink <shaynenguyen9@gmail.com>
2022-08-28 07:55:06 +0700

    chap 8

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,4 +9,13 @@ module SessionsHelper
         @current_user ||= User.find_by(id: session[:user_id])
         end
     end
+
+    def logged_in?
+        !current_user.nil?
+    end
+
+    def log_out
+        reset_session
+        @current_user = nil
+    end
 end
